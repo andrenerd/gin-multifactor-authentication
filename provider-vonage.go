bb9eba0daadcc01ede402f4064d5bd56f4596209
Andrenerd <andreid.31415@gmail.com>
2021-02-19 01:56:45 +0100

    poke vonage provider

--- provider-vonage.go
+++ provider-vonage.go
@@ -20,7 +20,23 @@ type UserVonageServiceProvider struct {
 	From string
 }
 
+type sendBody struct {
+	APIKey string `json:"api_key"`
+	APISecret string `json:"api_secret"`
+	// From string `json:"from"`
+	// Text string `json:"text"`
+	// To string `json:"to"`
+}
+
 // todo: make as goroutine?
 func (provider UserVonageServiceProvider) Send(to string, message string) error {
+	body := sendBody{
+		// APIKey: Key,
+		// APISecret: Secret,
+		// From: provider.From,
+		// To: to,
+		// Text: message,
+	}
+
 	return nil
 }
